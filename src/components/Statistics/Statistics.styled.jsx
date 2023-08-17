db7dcd2dd58b5dd2facf51304487093c5031be38
Yana-Levchyshyna <levchyshynayana@gmail.com>
2023-08-17 22:23:48 +0300

    commit

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -6,10 +6,25 @@ export const StatList = styled.ul`
   margin: 0 auto;
 
   border-radius: ${p => p.theme.radius.normal};
-  box-shadow: -1px -1px 1px #fff, 1px 1px 1px #babecc;
-  background: #4ecdc4;
-  background: -webkit-linear-gradient(to right, #556270, #4ecdc4);
-  background: linear-gradient(to right, #556270, #4ecdc4);
+
+ overflow: auto;
+  background: linear-gradient(to right, #556270 0%, #4ecdc4 100%);
+  animation: gradient 15s ease infinite;
+  background-size: 400% 400%;
+  background-attachment: fixed;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 0%;
+  }
+  50% {
+    background-position: 100% 100%;
+  }
+  100% {
+    background-position: 0% 0%;
+  }
+}
 
   box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.12),
     0px 2px 1px 0px rgba(0, 0, 0, 0.08), 0px 3px 1px 0px rgba(0, 0, 0, 0.1);
